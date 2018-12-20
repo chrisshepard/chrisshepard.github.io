f9e7129bdc1158c99f1335524ea26075bd108d5e
chrisshepard <43590660+chrisshepard@users.noreply.github.com>
2018-12-20 02:32:36 -0500

    build environment
    create-react-app
    SMACCS sass folders
    media folder

---
+++ my-app/src/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/app.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Christopher Shepard</h1>
+
+    );
+  }
+}
+
+export default App;
